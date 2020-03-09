70d91c17fb9450be71722656eded95d1f6dda632
Tetsuo Koyama <tkoyama010@gmail.com>
2020-03-09 14:50:52 +0900

    :new: test_title_overline

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,12 @@
+import rst2rst2
 import unittest
+
 
 class TestTitle(unittest.TestCase):
     def test_title_overline():
         title_string = "==\nAAAAA\n=="
-        self.assertFalse(check_title_overline(title_string=title_string))
+        self.assertFalse(rst2rst2.check_title_overline(title_string=title_string))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
