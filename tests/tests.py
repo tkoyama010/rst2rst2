8e231896ca97df38ed5bedc88641869cd4f9860f
Tetsuo Koyama <tkoyama010@gmail.com>
2020-03-09 19:59:10 +0900

    :new: test_title_overline

--- tests/tests.py
+++ tests/tests.py
@@ -3,7 +3,7 @@ import unittest
 
 
 class TestTitle(unittest.TestCase):
-    def test_title_overline():
+    def test_title_overline(self):
         title_string = "==\nAAAAA\n=="
         self.assertFalse(rst2rst2.check_title_overline(title_string=title_string))
 
